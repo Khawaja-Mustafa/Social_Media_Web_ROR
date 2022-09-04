bcaaf994de23f1720aeee4928d473f6ff0cd26bc
Khawaja Mustafa <khawajamustafa12@gmail.com>
2022-09-04 17:49:05 +0500

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,11 +52,13 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
+    # yeh add kia sath format.js
     @post.destroy
 
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
+      format.js   { render :layout => false }
     end
   end
 
