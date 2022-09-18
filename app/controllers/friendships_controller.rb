7adfc8921217617d915feef0c9bedf9eda263a10
Khawaja Mustafa <khawajamustafa12@gmail.com>
2022-09-18 12:35:30 +0500

    fixes

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ class FriendshipsController < ApplicationController
     @friendship = current_account.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
       flash[:notice] = "Added friend."
-      # redirect_to current_account
+      redirect_to profile_account_path(current_account)
     else
       flash[:error] = "Unable to add friend."
       redirect_to root_url
